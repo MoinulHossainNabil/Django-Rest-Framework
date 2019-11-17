5975b3f94a01f6b433b2eb62887844eeb1ff41c5
MoinulHassinaNabil <nnaabill@gmail.com>
2019-11-17 07:26:55 -0700

    set up the project created a model and checked the get and post request through api using serializer

---
+++ rest_api/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = Post.objects.all()
+        serializer = PostSerializer(data, many=True)
+        
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+
+        
+
+
+# def get(request):
+#     data = {
+#         'name': 'nabil',
+#         'id': 'loser'
+#     }
+#     return JsonResponse(data)
